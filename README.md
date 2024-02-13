f13d750fa853cb2acb3e9361c1b93827be70505d
ksantos <kepasantosruiz@gmail.com>
2024-02-13 10:18:06 +0100

    Add files via upload

---
+++ README.md
@@ -0,0 +1,80 @@
+# Building a NodeJS container image with Cloud-Native Buildpacks
+
+This project shows how to build a NodeJS container image with
+[Cloud-Native Buildpacks](https://buildpacks.io) (CNB) and
+[Paketo Buildpacks](https://paketo.io).
+
+You don't need to write a `Dockerfile` anymore: using CNB you get
+secured up-to-date container images out of your source code.
+
+You don't need to care about the runtime environment (is my NodeJS runtime up-to-date?):
+CNB will automatically provision dependencies and configure your container.
+
+## How to use it?
+
+[Download and install the `pack` CLI](https://github.com/buildpacks/pack/releases).
+You'll need a Docker daemon running to build container images.
+
+Use the Paketo builder:
+```bash
+$ pack config default-builder gcr.io/paketo-buildpacks/builder:base
+```
+
+You're now ready to use CNB with Paketo.
+
+Run this command to build a container image:
+```bash
+$ pack build myorg/cnb-nodejs
+...
+Successfully built image myorg/cnb-nodejs
+```
+
+After a couple of minutes, your container image will be published 
+to your local Docker daemon:
+```bash
+$ docker image ls | grep  cnb-nodejs
+REPOSITORY                             TAG                  IMAGE ID    
+myorg/cnb-nodejs                       latest               b1a0d242e5ec
+```
+
+You can run this container right away:
+```bash
+$ docker run --rm -p 8080:8080/tcp myorg/cnb-nodejs
+
+> cnb-nodejs@1.0.0 start /workspace
+> node server.js
+
+Server listening on port 8080
+```
+
+## Deploying to Kubernetes
+
+Use these Kubernetes descriptors to deploy this app to your cluster:
+```bash
+$ kubectl apply -f k8s
+```
+
+This app will be deployed to namespace `cnb-nodejs`:
+```bash
+kubectl -n cnb-nodejs get pod,deployment,svc
+NAME                       READY   STATUS    RESTARTS   AGE
+pod/app-7c7957cb94-lpmk8   1/1     Running   0          35m
+
+NAME                        READY   UP-TO-DATE   AVAILABLE   AGE
+deployment.extensions/app   1/1     1            1           35m
+
+NAME          TYPE           CLUSTER-IP      EXTERNAL-IP      PORT(S)        AGE
+service/app   LoadBalancer   10.100.200.35   35.187.115.254   80:30355/TCP   35m
+```
+
+## Contribute
+
+Contributions are always welcome!
+
+Feel free to open issues & send PR.
+
+## License
+
+Copyright &copy; 2022 [VMware, Inc. or its affiliates](https://vmware.com).
+
+This project is licensed under the [Apache Software License version 2.0](https://www.apache.org/licenses/LICENSE-2.0).
---
+++ server.js
@@ -0,0 +1,91 @@
+/*
+ * Copyright (c) 2022 VMware, Inc. or its affiliates
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+const fs = require('fs')
+const PropertiesReader = require('properties-reader')
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 8080
+
+var osName = "";
+if(fs.existsSync("/etc/os-release")) {
+  props = PropertiesReader("/etc/os-release")
+  osName = props.get("PRETTY_NAME").replace(/\"/g, "")
+}
+
+const info = {
+  node: `Node ${process.version}`,
+  os: osName
+}
+
+const greetings = process.env.GREETINGS || 'Hello world!';
+
+app.get('/greetings', function (req, res) {
+  res.setHeader("Content-Type", "text/plain");
+  res.end(greetings);
+})
+
+app.get('/info', function (req, res) {
+  res.setHeader("Content-Type", "application/json");
+  res.end(info)
+})
+
+var opensslVersion = "";
+const os = require('os');
+const opensslVersionOutput = require('child_process').execSync("openssl version -a").toString();
+const opensslVersionLines = opensslVersionOutput.split(os.EOL);
+opensslVersion = opensslVersionLines[0].trim();
+if(!opensslVersionLines[1].includes("not available")) {
+  opensslVersion += (" " + opensslVersionLines[1].trim());
+}
+
+const Prometheus = require('prom-client');
+const promReg = new Prometheus.Registry();
+
+const appInfo = new Prometheus.Counter({
+  name: 'app_info_total',
+  help: 'Get application info',
+  labelNames: ["application_name", "nodejs_version", "openssl_version"]
+});
+appInfo.labels("cnb-nodejs", info.node, opensslVersion).inc();
+promReg.registerMetric(appInfo);
+
+app.get('/metrics', (req, res) => {
+  res.set('Content-Type', promReg.contentType);
+  res.end(promReg.metrics());
+});
+
+const appDataDir = process.env.APP_DATA || '/app-data';
+if(fs.existsSync(appDataDir)) {
+  app.use(express.static(appDataDir));
+}
+
+app.use(express.static('static'));
+const server = app.listen(port, function () {
+  console.log(`Server listening on port ${port}`)
+})
+
+function shutDown() {
+  server.close(() => {
+      process.exit(0);
+  });
+  setTimeout(() => {
+      process.exit(1);
+  }, 10000);
+}
+
+process.on('SIGTERM', shutDown);
+process.on('SIGINT', shutDown);
